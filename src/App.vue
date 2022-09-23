ca8f3abe390bc080465d0459d2ab5f1f1ae6ca1e
hippo <hippo@potam.us>
2022-09-23 22:48:12 +0300

    model

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<!--5, two-way binding with full and short syntax -->
+<template>
+  <input v-bind:value="msg" v-on:input="onInput" placeholder="Enter text">
+  <p>{{msg}}</p>
+  <input :value="msg" @input="onInput" placeholder="Enter text">
+  <p>{{msg}}</p>
+  <input v-model="msg" placeholder="Enter text">
+  <p>{{msg}}</p>
+</template>
+
+<script>
+export default{
+  data(){
+    return {
+      msg: ""
+    }
+  },
+  methods: {
+    onInput(e){
+      this.msg = e.target.value
+    }
+  }
+}
+</script>
