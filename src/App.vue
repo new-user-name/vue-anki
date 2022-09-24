406f9470001da994c225340da537e553aad157dc
new-user-name <latexpostcard@gmail.com>
2022-09-24 18:54:13 +0300

    changed email

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <!--6, if-else toggle -->
 <template>
-  <h1 v-if='awesome'>The Vue is awesome</h1>
+  <h1 v-if="fat">Hippos are fat</h1>
   <h1 v-else>Oh no</h1>
   <button @click="toggle">Toggle</button>
 </template>
 
 <script>
-export default{
+export default {
   data(){
     return {
-      awesome: true
+      fat: true
     }
   },
-  methods: {
+  methods : {
     toggle(){
-      this.awesome = !this.awesome
+      this.fat = !this.fat
     }
   }
 }
