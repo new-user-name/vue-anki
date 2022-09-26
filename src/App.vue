0995cec488fa44ca376bd7176753912c9476ac2d
new-user-name <latexpostcard@gmail.com>
2022-09-27 00:47:55 +0300

    computed

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,55 @@
+<!--8, computed properties-->
 <template>
   <form @submit.prevent="addTodo">
-    <input v-model="newTodo">
+    <input v-model="textTodo">
     <button>Add todo</button>
   </form>
   <ul>
-    <li v-for="todo in todos" :key="todo.id">
-      {{todo.value}}
+    <li v-for="todo in filteredTodos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done">
+      <span :class="{done : todo.done}">{{todo.text}}</span>
       <button @click="removeTodo(todo)">X</button>
     </li>
   </ul>
+  <button @click="hideTodos = !hideTodos">{{hideTodos ? 'Hide completed' : 'Show all'}}</button>
 </template>
 
 <script>
- let id = 0
- export default {
-   data(){
-     return {
-       newTodo: '',
-       todos: [
-         {id: id++, value: "Hippos"},
-         {id: id++, value: "Potamuses"},
-         {id: id++, value: "Elephants"}
-       ]
-     }
-   },
-   methods: {
-     addTodo(){
-       this.todos.push({id: id++, value: this.newTodo})
-     },
-     removeTodo(todo){
-       this.todos = this.todos.filter(o => o !== todo)
-     }
-   }
- }
+
+let id = 0
+
+export default {
+  data() {
+    return {
+      textTodo: '',
+      hideTodos: false,
+      todos: [
+        {id: id++, text: "Hippo", done: false},
+        {id: id++, text: "Potamus", done: true},
+        {id: id++, text: "Elephant", done: false}
+      ]
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({id: id++, text: this.textTodo, done: false})
+    },
+    removeTodo(todo) {
+      this.todos = this.todos.filter(o => o !== todo)
+    }
+  },
+
+  computed: {
+    filteredTodos() {
+      return this.hideTodos ? this.todos : this.todos.filter (o => !o.done)
+    }
+  }
+
+}
 </script>
+
+<style>
+  .done {
+    text-decoration: line-through;
+  }
+</style>
---
+++ src/App7.vue
@@ -0,0 +1,37 @@
+<!--7, adding and removing todos, cycles, two-way binding and filtering-->
+<template>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo">
+    <button>Add todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{todo.value}}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+ let id = 0
+ export default {
+   data(){
+     return {
+       newTodo: '',
+       todos: [
+         {id: id++, value: "Hippos"},
+         {id: id++, value: "Potamuses"},
+         {id: id++, value: "Elephants"}
+       ]
+     }
+   },
+   methods: {
+     addTodo(){
+       this.todos.push({id: id++, value: this.newTodo})
+     },
+     removeTodo(todo){
+       this.todos = this.todos.filter(o => o !== todo)
+     }
+   }
+ }
+</script>
