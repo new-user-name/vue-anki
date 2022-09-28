3d5400dba5098a67f0ba7e7746904a3df3846d9c
new-user-name <latexpostcard@gmail.com>
2022-09-28 04:09:03 +0300

    async

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,43 @@
-<!--8, computed properties-->
+<!--10 async request and watchers-->
+
 <template>
-  <form @submit.prevent="addTodo">
-    <input v-model="textTodo">
-    <button>Add todo</button>
-  </form>
-  <ul>
-    <li v-for="todo in filteredTodos" :key="todo.id">
-      <input type="checkbox" v-model="todo.done">
-      <span :class="{done : todo.done}">{{todo.text}}</span>
-      <button @click="removeTodo(todo)">X</button>
-    </li>
-  </ul>
-  <button @click="hideTodos = !hideTodos">{{hideTodos ? 'Hide completed' : 'Show all'}}</button>
+  <p>Todo id {{ todoId }}</p>
+  <button @click="todoId++">Fetch next todo</button>
+  <p v-if="!todoData">Loading...</p>
+  <pre v-else>{{ todoData }}</pre>
+
 </template>
 
 <script>
-
-let id = 0
-
 export default {
   data() {
     return {
-      textTodo: '',
-      hideTodos: false,
-      todos: [
-        {id: id++, text: "Hippo", done: false},
-        {id: id++, text: "Potamus", done: true},
-        {id: id++, text: "Elephant", done: false}
-      ]
-    }
-  },
-  methods: {
-    addTodo() {
-      this.todos.push({id: id++, text: this.textTodo, done: false})
-    },
-    removeTodo(todo) {
-      this.todos = this.todos.filter(o => o !== todo)
+      todoId: 1,
+      todoData: null
     }
   },
 
-  computed: {
-    filteredTodos() {
-      return this.hideTodos ? this.todos : this.todos.filter (o => !o.done)
+  mounted() {
+    this.fetchData()
+  },
+
+  watch: {
+    todoId() {
+      this.fetchData()
+    }
+  },
+
+  methods: {
+    async fetchData() {
+      this.todoData = null
+      const res = await fetch(
+          `https://jsonplaceholder.typicode.com/todos/${this.todoId}`
+      )
+      this.todoData = await res.json()
     }
   }
 
 }
 </script>
 
-<style>
-  .done {
-    text-decoration: line-through;
-  }
-</style>
+
